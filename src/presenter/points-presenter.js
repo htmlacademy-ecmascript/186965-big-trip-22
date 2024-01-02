b94cbe235c23fefdabf9f1a4ea56d22922a2cfa4
Victoria <vitagood.92@list.ru>
2024-01-02 21:38:53 +0300

    Выделила функцию по создаю точки маршрута в отдельную функцию

--- src/presenter/points-presenter.js
+++ src/presenter/points-presenter.js
@@ -1,6 +1,6 @@
 import SortView from '../view/sort-view.js';
 import PointListView from '../view/point-list-view.js';
-import PointEditView from '../view/point-edit-view.js';
+// import PointEditView from '../view/point-edit-view.js';
 import PointView from '../view/point-view.js';
 
 import {render} from '../framework/render.js';
@@ -24,24 +24,22 @@ export default class PointsPresenter {
   init() {
     this.#tripPoints = [...this.#points.get()];
 
-
     render(new SortView(), this.#pointsContainer);
     render(this.#pointsBoard, this.#pointsContainer);
 
-    render(new PointEditView({
-      point: this.#tripPoints[0],
-      offers: this.#offers.getOfferByType(this.#tripPoints[0].type),
-      destination: this.#destinations.getDestinationById(this.#tripPoints[0].id)
-    }), this.#pointsBoard.element);
-
     for (let i = 0; i < this.#tripPoints.length; i++) {
-      render(new PointView(
-        {
-          point: this.#tripPoints[i],
-          offers: this.#offers.getOfferByType(this.#tripPoints[i].type),
-          destination:  this.#destinations.getDestinationById(this.#tripPoints[i].destination)
-        }), this.#pointsBoard.element);
+      this.#renderPoint(this.#tripPoints[i]);
     }
+  }
 
+
+  #renderPoint(point) {
+    const pointComponent = new PointView({
+      point,
+      offers: this.#offers.getOfferByType(point.type),
+      destination: this.#destinations.getDestinationById(point.destination)
+    });
+
+    render(pointComponent, this.#pointsBoard.element);
   }
 }
