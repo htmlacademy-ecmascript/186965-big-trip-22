440bb6d37ba1e94d8786ccb58dce424613d0126d
Victoria <vitagood.92@list.ru>
2024-01-03 22:58:21 +0300

    Города в шапке меняются в зависимости от точек

--- src/view/header/trip-info-view.js
+++ src/view/header/trip-info-view.js
@@ -1,6 +1,6 @@
 import AbstractView from '../../framework/view/abstract-view.js';
 import { humanizeDate } from '../../utils/point.js';
-import { START_DATE_FORMAT, MAX_VISIBLE_CITIES_COUNT, MULTIPLE_DOTS } from '../../const.js';
+import { START_DATE_FORMAT, MULTIPLE_DOTS } from '../../const.js';
 
 const createTripInfoTemplate = ({ points, destinations }) => {
   const pointsStartDate = humanizeDate(points[0].dateFrom, START_DATE_FORMAT);
@@ -36,20 +36,21 @@ export default class TripInfoView extends AbstractView {
   #getDestinationsNames() {
 
     const startCity = this.#destinations.find((destination) => destination.id === this.#points[0].destination);
-    let middleCities = this.#destinations.find((destination) => destination.id === this.#points[1]?.destination);
-    const endCity = this.#destinations.find((destination) => destination.id === this.#points[this.#points.length - 1].destination);
 
-    if (this.#points.length > MAX_VISIBLE_CITIES_COUNT) {
-      middleCities = MULTIPLE_DOTS;
-    }
+    const middleCities = this.#destinations.find((destination) => destination.id === this.#points[1]?.destination);
+
+    const endCity = this.#destinations.find((destination) => destination.id === this.#points[this.#points.length - 1]?.destination);
+
 
     switch (this.#points.length) {
       case 1:
         return startCity.name;
       case 2:
+        return `${startCity.name} &mdash; ${endCity.name}`;
+      case 3:
         return `${startCity.name} &mdash; ${middleCities.name} &mdash; ${endCity.name}`;
       default:
-        return `${startCity.name} &mdash; ${middleCities.name} &mdash; ${endCity.name}`;
+        return `${startCity.name} &mdash; ${MULTIPLE_DOTS} &mdash; ${endCity.name}`;
     }
   }
 }
